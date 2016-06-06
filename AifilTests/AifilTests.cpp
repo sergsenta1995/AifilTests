8bcd48380f4bdaaa9c0bb7932bd43840e0874613
sergsenta1995 <sergsenta1995@gmail.com>
2016-06-06 21:55:13 +0400

    add question2

--- AifilTests/AifilTests.cpp
+++ AifilTests/AifilTests.cpp
@@ -8,7 +8,6 @@
 
 using namespace std;
 
-// Вопрос 1
 namespace Question1
 {
 	vector<int> Factorization(int number)
@@ -70,11 +69,51 @@ namespace Question1
 	}
 }
 
+namespace Question2
+{	
+	template <class T>
+	vector<pair<int, int>> SplitVector(vector<T> vector_, int parts_number_)
+	{
+		vector<pair<int, int>> indexes_splited_vector;
+		int length_part = vector_.size() / parts_number_;
+		// Общий остаток частей после деления вектора на части
+		// (пусть vector_.size() = 8, parts_number_ = 5, тогда
+		// total_parts_remainder = 3;
+		// left_parts_remainder = 1;
+		// right_parts_remainder = 2;
+		int total_parts_remainder = vector_.size() - (length_part * parts_number_);		
+		int right_remainder = total_parts_remainder / 2;
+		int left_remainder = total_parts_remainder - right_remainder;
+		
+		for (int i = right_remainder; i < vector_.size() - left_remainder; i += length_part)
+			indexes_splited_vector.push_back(make_pair(i,i+length_part-1));
 
+		return indexes_splited_vector;
+	}
+
+	void OutputToConsole()
+	{		
+		vector<pair<int, int>> result;
+		vector<int> input_vector{1, 2, 3, 4, 5, 6, 7, 8, 9};
+		char *n = new char[10];
+		cin >> n;
+		result = SplitVector(input_vector, atoi(n));
+		// HACK: Страшные лямбды :(
+		std::for_each(
+			result.begin(),
+			result.end(),
+			[](pair<int, int> pairs)
+			{
+				cout << pairs.first << " " << pairs.second << endl;;
+			
+			}
+		);
+	}
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	Question1::OutputToConsole();
+	Question2::OutputToConsole();
 		
 
 	return 0;
